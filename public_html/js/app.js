19a8f13958452015f6fc8841d767998bbeaea90b
adil1994 <bouktaib.adil1@gmail.com>
2016-06-26 14:36:32 +0000

    Latest Update
    - Todo show error messages

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -17,7 +17,7 @@ tinzaApp.controller('headerController', function ($scope, ngDialog, $rootScope) 
             $rootScope.x = 0;
         }
     };
-    
+
     $scope.openSigninDialog = function () {
         alert("Hello");
         if ($rootScope.x === 0) {
@@ -37,28 +37,18 @@ tinzaApp.controller('headerController', function ($scope, ngDialog, $rootScope) 
 });
 tinzaApp.controller('signupController', function ($scope, ngDialog, $rootScope) {
     $rootScope.x = 0;
+    $scope.re = /^[a-zA-Z]\w{3,14}$/;
+});
 
-    $scope.verifyLastFirstname = function () {
-        if ($rootScope.x === 0) {
-            alert("dqdsqdqs");
-            $rootScope.x = 1;
-        } 
+tinzaApp.directive('ngEnter', function () {
+    return function (scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                scope.$apply(function (){
+                    scope.$eval(attrs.ngEnter);
+                });
+                event.preventDefault();
+            }
+        });
     };
-    
-    $scope.openSigninDialog = function () {
-        alert("Hello");
-        if ($rootScope.x === 0) {
-            ngDialog.open({
-                template: 'partials/signin.html', className: 'ngdialog-theme-default'
-            });
-            $rootScope.x = 1;
-
-        } else {
-            ngDialog.close({
-                template: 'partials/signin.html', className: 'ngdialog-theme-default'
-            });
-            $rootScope.x = 0;
-        }
-    };
-
 });
