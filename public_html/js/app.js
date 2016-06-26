85adba2a721218f25154ac74f373b5da70d8e56b
adil1994 <bouktaib.adil1@gmail.com>
2016-06-26 18:53:56 +0000

    Last Update
    To be send to the company

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,6 +1,29 @@
-var tinzaApp = angular.module('tinzaApp', ['ngDialog']);
+var tinzaApp = angular.module('tinzaApp', ['ngRoute' , 'ngAnimate' , 'ngDialog']);
+
+tinzaApp.config(['$routeProvider' , function($routeProvider){
+
+$routeProvider
+    .when('/home' , {
+      templateUrl: 'index.html' ,
+      controller: 'headerController'
+    })
+    .when('/singup' , {
+    templateUrl: 'partials/singUp.html' ,
+    controller: 'signupController'
+    })
+    .when('/singin' , {
+    templateUrl: 'partials/singin.html' ,
+    controller: 'headerController'
+    })
+
+
+}]);
+
+
 
 tinzaApp.controller('headerController', function ($scope, ngDialog, $rootScope) {
+
+
     $rootScope.x = 0;
 
     $scope.openSignupDialog = function () {
@@ -37,7 +60,9 @@ tinzaApp.controller('headerController', function ($scope, ngDialog, $rootScope) 
 });
 tinzaApp.controller('signupController', function ($scope, ngDialog, $rootScope) {
     $rootScope.x = 0;
-    $scope.re = /^[a-zA-Z]\w{3,14}$/;
+    $scope.password='';
+    $scope.paswordstrenght=0;
+    $scope.re = "^(?=.{6,20}$)(?=.*\d)(?=.*[a-zA-Z]).*$";
 });
 
 tinzaApp.directive('ngEnter', function () {
@@ -51,4 +76,78 @@ tinzaApp.directive('ngEnter', function () {
             }
         });
     };
+});
+
+
+
+
+
+tinzaApp.directive('checkStrength', function () {
+
+    return {
+        replace: false,
+        restrict: 'EACM',
+        link: function (scope, iElement, iAttrs) {
+
+            var strength = {
+                colors: ['#F00', '#F90', '#FF0', '#9F0', '#0F0'],
+                mesureStrength: function (p) {
+
+                    var _force = 0;
+                    var _regex = /[$-/:-?{-~!"^_`\[\]]/g;
+
+                    var _lowerLetters = /[a-z]+/.test(p);
+                    var _upperLetters = /[A-Z]+/.test(p);
+                    var _numbers = /[0-9]+/.test(p);
+                    var _symbols = _regex.test(p);
+
+                    var _flags = [_lowerLetters, _upperLetters, _numbers, _symbols];
+                    var _passedMatches = $.grep(_flags, function (el) { return el === true; }).length;
+
+                    _force += 2 * p.length + ((p.length >= 10) ? 1 : 0);
+                    _force += _passedMatches * 10;
+
+                    // penality (short password)
+                    _force = (p.length <= 6) ? Math.min(_force, 10) : _force;
+
+                    // penality (poor variety of characters)
+                    _force = (_passedMatches == 1) ? Math.min(_force, 10) : _force;
+                    _force = (_passedMatches == 2) ? Math.min(_force, 20) : _force;
+                    _force = (_passedMatches == 3) ? Math.min(_force, 40) : _force;
+
+                    return _force;
+
+                },
+                getColor: function (s) {
+
+                    var idx = 0;
+                    if (s <= 10) { idx = 0; }
+                    else if (s <= 20) { idx = 1; }
+                    else if (s <= 30) { idx = 2; }
+                    else if (s <= 40) { idx = 3; }
+                    else { idx = 4; }
+
+                    return { idx: idx + 1, col: this.colors[idx] };
+
+                }
+            };
+
+            scope.$watch(iAttrs.checkStrength, function () {
+                if (scope.password === '') {
+                    iElement.css({ "display": "none"  });
+                } else {
+                    var c = strength.getColor(strength.mesureStrength(scope.password));
+                    iElement.css({ "display": "inline" });
+                    scope.paswordstrenght=c.idx;
+                    iElement.children('li')
+                        .css({ "background": "#DDD" })
+                        .slice(0, c.idx)
+                        .css({ "background": c.col });
+                }
+            });
+
+        },
+        template: '<li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li><li class="point"></li>'
+    };
+
 });
